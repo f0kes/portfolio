00a60224d736f8811b69575f740e1d6e38b8f3a3
f0kes <f0kesppokes@gmail.com>
2024-09-30 11:18:55 +0300

    +

--- frontend/src/lib/components/SocialLinks.svelte
+++ frontend/src/lib/components/SocialLinks.svelte
@@ -47,12 +47,26 @@
             />
         </svg>
     </a>
+    <a
+        href="mailto:f0kesppokes@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-4xl text-red-500 hover:text-red-400 transition duration-300 icon-link"
+    >
+        <svg
+            class="w-8 h-8 fill-current"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"
+            />
+        </svg>
+    </a>
 </div>
 
 <style>
-  :global(.dark) .icon-link svg {
-    filter: invert(1);
-  }
+  
   
   @media (prefers-color-scheme: dark) {
     .icon-link svg {
@@ -63,4 +77,3 @@
     }
   }
 </style>
-
